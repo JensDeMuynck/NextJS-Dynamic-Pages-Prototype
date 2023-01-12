ad4cf4112b8e45762b4eedc69630eb979c8605d8
Jens De Muynck <demuynck.jens@gmail.com>
2023-01-12 22:54:11 +0100

    commented google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,9 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					{/* <link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-					<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=Signika:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+					<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=Signika:wght@300;400;500;600;700&display=swap" rel="stylesheet" /> */}
 				</Head>
 				<body>
 					<Main />
