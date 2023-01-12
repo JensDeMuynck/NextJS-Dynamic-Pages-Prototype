d8c13fbca1bfd5d03139da6ab8cbd27ca3310547
Jens De Muynck <demuynck.jens@gmail.com>
2023-01-12 22:46:21 +0100

    new page + seo test

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,5 +1,4 @@
-// import fs from 'fs';
-// import path from 'path';
+import Head from 'next/head';
 
 import Default from '@/components/templates/Default';
 import Contact from '@/components/templates/Contact';
@@ -10,18 +9,25 @@ const templates = {
 }
 
 export default function DynamicPage(props) {
-	const { template } = props.pageData;
+	const { title, description, template } = props.pageData;
 
 	const Template = templates[template];
-	return <Template {...props} />;
+	return (
+		<>
+			<Head>
+				<title>{title}</title>
+				<meta name="description" content={description} />
+			</Head>
+			<Template {...props} />
+		</>
+	);
 }
 
 export async function getServerSideProps(context) {
 	// const data = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'cms', 'pages.json'), 'utf-8'));
 
-	const res = await fetch('https://raw.githubusercontent.com/JensDeMuynck/NextJS-Dynamic-Pages-Prototype/master/cms/pages.json?token=GHSAT0AAAAAAB5NQ3O7HZVBAZVX4G42L65KY6APORQ');
+	const res = await fetch('https://raw.githubusercontent.com/JensDeMuynck/NextJS-Dynamic-Pages-Prototype/master/cms/pages.json');
 	const data = await res.json();
-	console.log(data);
 
 	const { slug } = context.params;
 	const pageData = data.pages.find((page) => {
--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,6 @@ function MyApp({ Component, pageProps }) {
 MyApp.getInitialProps = async (appContext) => {
     // calls page's `getInitialProps` and fills `appProps.pageProps`
     const appProps = await App.getInitialProps(appContext);
-	console.log({appContext});
 
     return { ...appProps };
 };
